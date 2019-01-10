a2f4b4dc71193edc2c954f8adf63dcd740ab051b
sanket <sskulkarni.1990@gmail.com>
2019-01-10 08:39:04 +0530

    add observer

---
+++ Observer/CheckCustomerIpObserver.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Krishaweb\LocaleRedirect\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use GeoIp2\Database\Reader as GeoIP2Reader;
+
+
+class CheckCustomerIpObserver implements ObserverInterface{
+
+	public function execute(\Magento\Framework\Event\Observer $observer){
+					
+	}
+}
